6f6e50650f0dbb113d6396eb80379cd697a65507
Armand Gilles <arm.gilles@gmail.com>
2015-01-05 19:36:34 +0100

    Add new graph for dashbooad vision

--- Plot_the_city.py
+++ Plot_the_city.py
@@ -8,6 +8,8 @@ import os
 import numpy as np
 from nltk.corpus import stopwords
 from math import radians, cos, sin, asin, sqrt
+from string import capwords
+
 
 """""""""""""""""""""
 # Script http://overpass-turbo.eu/ :
@@ -82,7 +84,21 @@ def clean_stopwords(x, y):
     except:
         return('')
 
-
+def number_word(x):
+    """
+    Return number of word in a street name - 1 (type of street)
+    """
+    x = x.replace('É', 'e') # "É" can't be lower()
+    x = x.replace('-', ' ') # To split some funny way
+    x = x.replace("l'", "") # To better split and matching
+    try:
+        x = [w for w in x.split()]
+        return len(x)
+    except:
+        return(0)
+    
+    
+    
 stopw = stopwords.words('french')
 
 """""""""""""""""""""
@@ -224,7 +240,10 @@ data = data[['way_id', 'way_name', 'way_gender', 'node_lat', 'node_lon']]
 
 data['way_name']= data['way_name'].str.encode('utf-8')
 
-if len(data) > 10000:
+
+# To DO : Do we still need reduction of points ?
+
+if len(data) > 100000:
     # To reduce number of points by street
     print "Delete some points ..."
     
@@ -247,11 +266,36 @@ if len(data) > 10000:
     print 'Number line delete :' + str(len(index_to_delete)) + ' len data : ' +str(len(data))
         
 else:
-        pass
+    data['next_lat'] = data['node_lat'].shift(-1)
+    data['next_lon'] = data['node_lon'].shift(-1)
+    data['distance'] = data.apply(lambda row: haversine(row['node_lon'], row['node_lat'], row['next_lon'], row['next_lat']), axis=1)
 
 df_i = data[data['way_gender'] == 'Inconnu']
 df_m = data[data['way_gender'] == 'M']
 df_f = data[data['way_gender'] == 'F']
+
+# Analyse result by gender
+groupe_name_full = pd.DataFrame(data.groupby(['way_gender', 'way_name']).size())
+groupe_name_full.reset_index(inplace=True)
+groupe_name_full.columns = ['way_gender', 'way_name', 'count_way_id']
+groupe_name_full['Nbr'] = 1
+groupe_name = groupe_name_full[groupe_name_full['way_gender'] != 'Inconnu']
+
+groupe_gender_full = pd.DataFrame(groupe_name_full.groupby('way_gender').sum()).reset_index()
+groupe_gender = pd.DataFrame(groupe_name.groupby('way_gender').sum()).reset_index()
+groupe_gender_full['percent'] = groupe_gender_full['Nbr'].apply(lambda x: 100*x/float(groupe_gender_full.Nbr.sum()))
+groupe_gender['percent'] = groupe_gender['Nbr'].apply(lambda x: 100*x/float(groupe_name.Nbr.sum()))
+
+# Aalyse by street's type
+groupe_name['type'] = groupe_name['way_name'].apply(lambda x: clean_stopwords(x.encode('utf-8'), int(0)))
+groupe_name.type = groupe_name.type.apply(lambda x: capwords(x)) # For Maj
+groupe_type = pd.DataFrame(groupe_name.groupby(['type']).sum()).reset_index()
+groupe_type.sort('Nbr', ascending=False, inplace=True)
+top5_type = groupe_type.type[0:5].tolist()
+groupe_type_gender = pd.DataFrame(groupe_name.groupby(['type', 'way_gender']).sum()).reset_index()
+groupe_type_gender = groupe_type_gender[groupe_type_gender.type.isin(top5_type)]
+groupe_type_gender_m = groupe_type_gender[groupe_type_gender['way_gender'] == 'M']
+groupe_type_gender_f = groupe_type_gender[groupe_type_gender['way_gender'] == 'F']
 
 """""""""""""""""""""
 Matplotlib graph
@@ -262,18 +306,18 @@ fig = plt.figure(figsize=(12,8))
 #for index, grp, in city.groupby(['way_id']):
 #    plt.plot(grp.node_lon, grp.node_lat, color='g', alpha=0.35)
 for index, grp, in df_i.groupby(['way_id']):
-    plt.plot(grp.node_lon, grp.node_lat, color='k', alpha=0.4, linewidth=0.2, label=grp.way_name.unique())
+    plt.plot(grp.node_lon, grp.node_lat, color='k', alpha=0.75, linewidth=0.2, label=grp.way_name.unique())
     #plt.scatter(grp.node_lon, grp.node_lat, color='k', alpha=0.5, label=grp.way_name.unique(), s=0.2)
 for index, grp, in df_m.groupby(['way_id']):
-    plt.plot(grp.node_lon, grp.node_lat, color='b', alpha=0.4, linewidth=0.2, label=grp.way_name.unique())
+    plt.plot(grp.node_lon, grp.node_lat, color='b', alpha=0.75, linewidth=0.2, label=grp.way_name.unique())
     #plt.plot(grp.node_lon, grp.node_lat, color='b', alpha=0.65)
 for index, grp, in df_f.groupby(['way_id']):
-    plt.plot(grp.node_lon, grp.node_lat, color='m', alpha=0.4, linewidth=0.2, label=grp.way_name.unique())
+    plt.plot(grp.node_lon, grp.node_lat, color='m', alpha=0.75, linewidth=0.2, label=grp.way_name.unique())
     #plt.plot(grp.node_lon, grp.node_lat, color='m', alpha=0.65)
 
 plt.title("Bordeaux's gender streets")
 
-
+plt.savefig('test.pdf', format ='pdf' )
 plt.show()
 
 """
@@ -284,7 +328,7 @@ End Matplotlib graph
 
 
 """""""""""""""""""""
-Graph Plotly
+ Graph Plotly
 """""""""""""""""""""
 
 colors = dict(
@@ -326,10 +370,48 @@ for way, X in data.groupby(['way_gender', 'way_id']):
         make_trace(X, street_gender, color)  # append trace to data object
     )
 
-title = "My City's streets gender"
+# Histo for % of gender's street  
+bar_trace = Bar(
+        x=groupe_gender['way_gender'],
+        y=groupe_gender['percent'],
+        name = '%',
+        xaxis='x2',
+        yaxis='y2',
+        marker=Marker(
+            color=['#DC0671', '#5EB4E6']
+        )
+)
+
+data_plot.append(bar_trace)
+
+# Stack histo for street's type
+trace_type_gender_m = Bar(
+                            x=groupe_type_gender_m['type'],
+                            y=groupe_type_gender_m['Nbr'],
+                            name='M',
+                            xaxis='x3',
+                            yaxis='y3',
+                            marker=Marker(color='#5EB4E6')
+)
+
+trace_type_gender_f = Bar(
+                            x=groupe_type_gender_f['type'],
+                            y=groupe_type_gender_f['Nbr'],
+                            name='F',
+                            xaxis='x3',
+                            yaxis='y3',
+                            marker=Marker(color='#DC0671')
+)
+
+data_plot.append(trace_type_gender_m)
+data_plot.append(trace_type_gender_f)
+
+
+title = "My city's street gender v4"
 
 layout = Layout(
-    title=title,             # set plot title
+    title=title,
+    barmode='stack',
     xaxis=XAxis(
         autorange=True,
         showgrid=False,
@@ -337,7 +419,9 @@ layout = Layout(
         showline=False,
         autotick=True,
         ticks='',
-        showticklabels=False
+        showticklabels=False,
+        domain=[0, 0.85],
+        anchor='x1'
      ),
     yaxis=YAxis(
         autorange=True,
@@ -346,7 +430,31 @@ layout = Layout(
         showline=False,
         autotick=True,
         ticks='',
-        showticklabels=False
+        showticklabels=False,
+        domain=[0, 1],
+        anchor='y1'
+    ),
+    xaxis2=XAxis(
+        domain=[0.85, 1],
+        anchor='y2'
+    ),
+    yaxis2=YAxis(
+        domain=[0.7, 1],
+        anchor='x2'
+    ),
+    xaxis3=XAxis(
+        domain=[0.85, 1],
+        anchor='y3'
+    ),
+    yaxis3=YAxis(
+        domain=[0.25, 0.55],
+        anchor='x3'
+    ),
+    margin=Margin(
+        l=5,
+        r=5,
+        b=10,
+        t=80
     ),
     annotations=Annotations([
     Annotation(
@@ -361,8 +469,7 @@ layout = Layout(
         borderpad=4           # set border/text space (in pixels)
     )
     ])
-)    
-#plot_bgcolor='#F8FAEE'   # set plot color to grey
+)
 
 
 
@@ -375,11 +482,7 @@ fig['layout'].update(
     
     showlegend=False     # remove legend (info in hover)
 )
-"""
-    autosize=False,       # turn off autosize
-    width=650,            # plot width
-    height=500,           # plot height
-    """
+
 
 
 # Define a hover-text generating function (returns a list of strings)
@@ -398,11 +501,10 @@ for way, X in data.groupby(['way_gender', 'way_id']):
     fig['data'][i_trace].update(text=text)         # update trace i
     i_trace += 1                                   # inc. trace counter
 
-#py.plot(fig, filename="My City's streets gender") 
+
+py.plot(fig, filename="My cuty's street gender") 
 
 
 """"""""""""""""""""" 
-End Graph Plotly
+# End Graph Plotly
 """""""""""""""""""""
-
-
